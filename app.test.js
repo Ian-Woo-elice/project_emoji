da3693609165d189b1ca3dc3b2e5c7bebf5b6b44
Ian-Woo-elice <symphonie913@gmail.com>
2025-06-04 17:39:50 +0900

    Update emoji page title and copy behavior

---
+++ app.test.js
@@ -0,0 +1,31 @@
+import {TextEncoder, TextDecoder} from 'util';
+import {jest} from '@jest/globals';
+
+let showCopyIndicator;
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+describe('showCopyIndicator', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  beforeAll(() => {
+    global.TextEncoder = TextEncoder;
+    global.TextDecoder = TextDecoder;
+  });
+
+  test('creates and removes indicator', async () => {
+    document.body.innerHTML = '<div class="emoji-item"></div>';
+    ({ showCopyIndicator } = await import('./app.js'));
+    const emojiItem = document.querySelector('.emoji-item');
+    showCopyIndicator(emojiItem);
+    const indicator = emojiItem.querySelector('.copy-indicator');
+    expect(indicator).not.toBeNull();
+    expect(indicator.textContent).toBe('복사 완료');
+
+    jest.advanceTimersByTime(1000);
+    expect(emojiItem.querySelector('.copy-indicator')).toBeNull();
+  });
+});
